b4c520d8cd2be6edd334cab6b4130c467d846feb
Snack Man <lpardo1989@gmail.com>
2023-06-28 08:13:54 -0700

    minor updates to js files

--- index.js
+++ index.js
@@ -1,25 +1,16 @@
-// Inquirer (node package manager) import
 const inquirer = require("inquirer");
-
-// File system module (node package manager) import
 const fs = require("fs");
 
-// Importing classes from ./lib/shapes directory
 const { Triangle, Square, Circle } = require("./lib/shapes");
 
-// Function writes the SVG file using user answers from inquirer prompts
-function writeToFile(fileName, answers) {
-  // File starts as an empty string
+
+function newFile(fileName, answers) {
   let svgString = "";
-  // Sets width and height of logo container
   svgString =
     '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
-  // <g> tag wraps <text> tag so that user font input layers on top of polygon -> not behind
   svgString += "<g>";
-  // Takes user input for shape choice and inserts it into SVG file
   svgString += `${answers.shape}`;
 
-  // Conditional check takes users input from choices array and then adds polygon properties and shape color to SVG string
   let shapeChoice;
   if (answers.shape === "Triangle") {
     shapeChoice = new Triangle();
@@ -32,20 +23,18 @@ function writeToFile(fileName, answers) {
     svgString += `<circle cx="150" cy="115" r="80" fill="${answers.shapeBackgroundColor}"/>`;
   }
 
-  // <text> tag gives rise to text alignment, text-content/text-color taken in from user prompt and gives default font size of "40"
-  svgString += `<text x="150" y="130" text-anchor="middle" font-size="40" fill="${answers.textColor}">${answers.text}</text>`;
-  // Closing </g> tag
+ 
+  svgString += `<text x="150" y="130" text-anchor="middle" font-size="50" fill="${answers.textColor}">${answers.text}</text>`;
   svgString += "</g>";
-  // Closing </svg> tag
   svgString += "</svg>";
 
-  // Using file system module to generate svg file, takes in file name given in the promptUser function, the svg string, and a ternary operator which handles logging any errors, or a "Generated logo.svg" message to the console  
-  fs.writeFile(fileName, svgString, (err) => {
+  
+  fs.newFile(fileName, svgString, (err) => {
     err ? console.log(err) : console.log("Generated logo.svg");
   });
 }
 
-// This function utilizes inquirer .prompt to prompt the user to answer questions in the command line and save user input
+
 function promptUser() {
   inquirer
     .prompt([
@@ -85,7 +74,7 @@ function promptUser() {
         promptUser();
       } else {
         // Calling write file function to generate SVG file
-        writeToFile("logo.svg", answers);
+        newFile("logo.svg", answers);
       }
     });
 }
